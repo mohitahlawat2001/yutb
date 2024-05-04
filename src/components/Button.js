ff35fb3c4c05cbeb01c993210f1922b96979f1ea
Mohit ahlawat <65100859+mohitahlawat2001@users.noreply.github.com>
2024-05-04 14:50:26 +0000

    added button list on top of main container

---
+++ src/components/Button.js
@@ -0,0 +1,9 @@
+const Button = ({name}) =>{
+    return (
+        <div>
+            <button className="bg-gray-100  px-5 py-2 m-2 rounded-lg" > {name} </button>
+        </div>
+    )
+}
+
+export default Button;
--- src/components/Buttonlist.js
+++ src/components/Buttonlist.js
@@ -1,8 +1,13 @@
 import React from 'react'
-
+import Button from './Button';
+const list = ['all', 'web', 'mobile' ,'desktop', 'games', 'network', 'security', 'database', 'cloud', 'other']
 const Buttonlist = () => {
   return (
-    <div>Buttonlist</div>
+    <div className='flex flex-row'>
+      {list.map((item, index) => {
+        return <Button key={index} name={item} />
+      })}
+    </div>
   )
 }
 
