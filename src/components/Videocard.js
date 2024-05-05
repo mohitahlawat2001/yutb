345a0cf9b843decf4467a03926008251e3bfd4e7
Mohit ahlawat <65100859+mohitahlawat2001@users.noreply.github.com>
2024-05-05 07:18:34 +0000

    added videos in main video container using youtube api

--- src/components/Videocard.js
+++ src/components/Videocard.js
@@ -1,9 +1,22 @@
 import React from 'react'
 
-const Videocard = () => {
+const Videocard = ({info}) => {
+  console.log(info);
+  
+  const {snippet, statistics} = info;
+  const {title, channelTitle, thumbnails} = snippet;
+  const {viewCount, likeCount} = statistics;
   return (
-    <div>Videocard</div>
+    <div className='p-2 m-2 w-72 shadow-lg rounded-md' >
+      <img className='rounded-lg ' src={thumbnails.medium.url} alt={title} />
+      <h2 className='font-bold py-2 mx-2' >{title}</h2>
+      <ul>
+      <li>{channelTitle}</li>
+      <li>Views: {viewCount}</li>
+      <li>Likes: {likeCount}</li>
+      </ul>
+    </div>
   )
 }
 
-export default Videocard
+export default Videocard;
--- src/components/Videocontainer.js
+++ src/components/Videocontainer.js
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useEffect , useState} from 'react'
 import Videocard from './Videocard'
+import { YOUTUBE_VIDEO_API } from '../utils/constants'
 
 const Videocontainer = () => {
+    const [videos, setVideos] = useState([]);
+    useEffect(() => {
+        getVideos();
+    }, [])
+
+const getVideos = async () => {
+    try {
+        const response = await fetch(YOUTUBE_VIDEO_API);
+        const data = await response.json();
+        // console.log(data.items);
+        setVideos(data.items);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
   return (
-      <div>
-          <Videocard />
+    <div className='flex flex-wrap p-2 m-2 ' >
+        {videos[0]?.snippet?.title && videos.map(video => (
+            <Videocard key={video.id} info={video} />
+        ))}
     </div>
   )
 }
